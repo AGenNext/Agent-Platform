94672cab502c71f7b8ff74d3b6bec2d9a6a2892d
Chinmay Panda <chinmay@openautonomyx.com>
2026-06-25 01:39:38 +0530

    Add console API application

---
+++ internal-agent-console/apps/api/src/main/java/io/agennext/console/ConsoleApiApplication.java
@@ -0,0 +1,11 @@
+package io.agennext.console;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsoleApiApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ConsoleApiApplication.class, args);
+  }
+}
